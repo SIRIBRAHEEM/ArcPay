dcde8d6d88935c7e5367bab1f2ab22e1d890cc86
IBRAHIM YAHAYA <75159437+SIRIBRAHEEM@users.noreply.github.com>
2026-04-30 20:37:59 +0100

    Delete app/lib/circle.ts

--- app/lib/circle.ts
+++
@@ -1,7 +0,0 @@
-// app/lib/circle.ts
-import { initiateDeveloperControlledWalletsClient } from "@circle-fin/developer-controlled-wallets";
-
-export const circleClient = initiateDeveloperControlledWalletsClient({
-  apiKey: process.env.CIRCLE_API_KEY!,
-  entitySecret: process.env.CIRCLE_ENTITY_SECRET!,
-});
